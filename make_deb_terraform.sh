fa1c445a24c67fef949ca5eecfd591a33be24a58
Troy Jendra <troy@troyjendra.com>
2017-08-12 10:55:11 +1000

    Replace packagecloud with a github repo for the apt repository

--- make_deb_terraform.sh
+++ make_deb_terraform.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-# check the latest terraform version vs the latest package version
-# and if not the same, build a new package
+# Check the latest terraform version vs the latest packaged version.
+# If not the same, build a new package, and push to apt repo.
+
+# set the gpg home dir to be ./.gnupg
+GNUPGHOME=./.gnupg
+
+# set restrictive umask so gpg and ssh keys get correct permissions
+umask 0077
 
 # get latest version number
 RELEASES_PAGE="https://releases.hashicorp.com/terraform/"
@@ -23,7 +29,7 @@ echo
 echo 'importing hashicorp gpg key'
 KEYID="51852D87348FFC4C"
 URL_KEY="https://keybase.io/hashicorp/pgp_keys.asc"
-curl --silent "${URL_KEY}" | gpg --quiet --homedir ./.gnupg --import
+curl --silent "${URL_KEY}" | gpg --import
 if [ $? -ne 0 ]; then
   echo "Failed to import hashicorp gpg key!"
   exit 1;
@@ -31,7 +37,7 @@ fi
 echo
 
 echo 'verifying downloaded signature'
-gpg --homedir ./.gnupg --trusted-key "${KEYID}" --verify SHA256SUMS.sig SHA256SUMS
+gpg --trusted-key "${KEYID}" --verify SHA256SUMS.sig SHA256SUMS
 if [ $? -ne 0 ]; then
   echo "Failed to verify downloaded signature!"
   exit 1;
@@ -52,27 +58,65 @@ echo
 echo 'building linux amd64 package'
 fpm --input-type zip --output-type deb --prefix /usr/local/bin --name terraform --version "${LATEST_VERSION}" --architecture amd64 latest_linux_amd64.zip
 if [ $? -ne 0 ]; then
-  echo "failed to build the linux amd64 package!"
+  echo "Failed to build the linux amd64 package!"
   ls -l
   exit 1;
 fi
 echo
 
-# push using this script, instead of using .travis.yml, using travis
-# environment variables for the packagecloud details - as per
-# https://packagecloud.io/docs
-#
-# set the following travis environment variables:
-# - PACKAGECLOUD_REPO
-# - PACKAGECLOUD_TOKEN
-# - PACKAGECLOUD_USER
-echo 'pushing package to packagecloud'
-package_cloud push "${PACKAGECLOUD_USER}/${PACKAGECLOUD_REPO}/debian/jessie" "terraform_${LATEST_VERSION}_amd64.deb"
+# import apt repo signing key, replacing '_' with newline
+echo $REPO_TERRAFORM_GPG_KEY | tr '_' '\n' | gpg --allow-secret-key-import --import
+
+# write apt repo git ssh key, replacing '_' with newline
+echo $REPO_TERRAFORM_SSH_KEY | tr '_' '\n' > .id_rsa
+
+# configure ssh to use our ssh key
+export GIT_SSH_COMMAND='ssh -i .id_rsa -o StrictHostKeyChecking=no'
+
+echo 'cloning the deb repo'
+git clone git@github.com:tjend/repo_terraform.git
 if [ $? -ne 0 ]; then
-  echo "failed to push the linux amd64 package to packagecloud!"
+  echo "Failed to clone the deb repo!"
   exit 1;
 fi
 echo
+
+echo "adding repo files to repo if the don't already exist"
+for FILE in $(ls repo_files/); do
+  if [ ! -e "repo_terraform/${FILE}" ]; then
+    cp "repo_files/${FILE}" repo_terraform/
+  fi
+done
+echo
+
+echo 'adding new deb package using reprepro'
+reprepro --basedir ./repo_terraform --confdir ./reprepro/conf includedeb stable "terraform_${LATEST_VERSION}_amd64.deb"
+if [ $? -ne 0 ]; then
+  echo "Failed to add the new deb package using reprepro!"
+  gpg --list-secret-keys
+  exit 1;
+fi
+echo
+
+echo 'adding latest version file to repo'
+echo "${LATEST_VERSION}" > repo_terraform/LATEST
+if [ $? -ne 0 ]; then
+  echo "Failed to add latest version file to repo!"
+  exit 1;
+fi
+echo
+
+echo 'pushing the updated deb repo'
+export GIT_SSH_COMMAND='ssh -i ../.id_rsa -o StrictHostKeyChecking=no'
+cd repo_terraform && git add . && HOME=../gitconfig git commit -m "Add terraform ${LATEST_VERSION}" && git push
+if [ $? -ne 0 ]; then
+  echo "Failed to push the updated deb repo!"
+  exit 1;
+fi
+echo
+
+# cleanup
+rm -rf .gnupg .id_rsa
 
 echo 'Script finished successfully!'
 exit 0
